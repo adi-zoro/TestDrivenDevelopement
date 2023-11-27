0b8b2030a88d918041da0b540a84787245a34ddf
adi-zoro <150170922+adi-zoro@users.noreply.github.com>
2023-11-27 21:26:01 +0000

    Update ColourTableTest.java
    added new unit test.

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -18,13 +18,19 @@ class ColourTableTest {
     }
 
 
-    @Test
+    @Test //test to check whether illegal argument exceptions are working or not.
     public void testInvalidColorTableCreation() {
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(13)); // Palette size not a power of two
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(2048)); // Palette size exceeds limit
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(1)); // Palette size less than 2
     }
 
+
+    @Test
+    public void testAddColor() {
+        table.add(0xFF0000); // Adding red color
+        assertEquals(1, table.getAddedColorsSize());
+    }
 
 
 }
