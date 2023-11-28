39bafa15073622172fd0a289a75a9ffb341f7f5a
adi-zoro <150170922+adi-zoro@users.noreply.github.com>
2023-11-28 01:07:02 +0000

    generated javadoc

---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"ColourTable","l":"add(int)"},{"p":"<Unnamed>","c":"ColourTable","l":"ColourTable(int)","u":"%3Cinit%3E(int)"},{"p":"<Unnamed>","c":"ColourTable","l":"getAddedColorsSize()"}];updateSearchResults();
